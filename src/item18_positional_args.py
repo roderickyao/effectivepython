98a4d977f677b41f5d447abdd2e4201c08e69bf5
yhqs540 <366s540>
2018-05-13 15:21:33 -0400

    item 18

---
+++ src/item18_positional_args.py
@@ -0,0 +1,23 @@
+def log(message, values):
+    if not values:
+        print(message)
+    else:
+        values_str = ', '.join(str(x) for x in values)
+        print('%s: %s' % (message, values_str))
+
+
+def log1(message, *values):
+    if not values:
+        print(message)
+    else:
+        values_str = ', '.join(str(x) for x in values)
+        print('%s: %s' % (message, values_str))
+
+
+def main():
+    log('My numbers are', [1, 2])
+    log('Hi There', [])
+    log1('Hi There')
+
+
+main()
